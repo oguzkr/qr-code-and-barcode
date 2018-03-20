788591c1f7c4c81ec07fb730d01fee0c5ce253d9
Oğuz Karatoruk <oguzkaratoruk@yandex.com>
2018-03-20 10:11:34 +0300

    Initial Commit

---
+++ qr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qr
+//
+//  Created by Oğuz Karatoruk on 20.03.2018.
+//  Copyright © 2018 Oğuz Karatoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
